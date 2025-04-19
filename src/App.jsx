a755c554172d053f64fd93dcb1929655d0f5df64
ayale3 <ayanle876@gmail.com>
2025-04-19 14:28:28 +0300

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "@/components/ui/button";
 
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="bg-yellow-500 p-16">
-
-      
-     <main>
-
-     </main>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
     </div>
   );
 }
