6fb89d94b4032b3f32796574fc9ea05a916d8e74
ayale3 <ayanle876@gmail.com>
2025-04-19 14:38:33 +0300

    added components  header footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,30 @@ import { Button } from "@/components/ui/button";
 
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Admin } from "./pages/Admin";
+import { Booking } from "./pages/Booking";
+import { Payment } from "./pages/Payment";
+import { Passenger } from "./pages/Passenger";
+import { PaymentFailed } from "./pages/PaymentFailed";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="bg-yellow-500 p-16">
+    <div>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="admin" element={<Admin />} />
+          <Route path="booking" element={<Booking />} />
+          <Route path="booking/:booking-id" element={<Booking />} />
+          <Route path="payment" element={<Payment />} />
+          <Route path="passenger" element={<Passenger />} />
+          <Route path="payment-failed" element={<PaymentFailed />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="signup" element={<SignUp />} />
         </Routes>
       </main>
     </div>
