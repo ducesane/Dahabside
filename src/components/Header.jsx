26bafd7a14099c37308b093ba93fd5275caf2d78
ayale3 <ayanle876@gmail.com>
2025-04-20 16:21:16 +0300

    ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ export const Header = () => {
     <div className="max-w-full bg-[#05203c]">
       <header className="   mx-auto  h-24 flex   px-24  ">
         <nav className="w-full flex  items-center justify-between">
+          
           {/* nav left */}
           <div className=" ">
             <h1 className="text-white  text-3xl font-semibold" >Dahabside</h1>
