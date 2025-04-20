0a36cb57228e27a4e41350e49ce307efdc3e9cc8
ayale3 <ayanle876@gmail.com>
2025-04-20 06:49:52 +0300

    ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 export const Header = () => {
   return (
-    <div className="bg-backgroundLight shadow">
+    <div className="bg-[#05203c] shadow">
       <div className="max-w-xl max-auto px-4 sm:px-6 lg:px-8 flex justify-between ">
         <div className="bg-red-100 text-white p-4 rounded">
           Dark blue background with white text
@@ -11,6 +11,9 @@ export const Header = () => {
         <Button variant="secondary" size="lg" className="mt-2">
           Hello
         </Button>
+        <div className="bg-[#05203c] text-white p-4 rounded-lg">
+  This background is baluu!
+</div>
         header
       </div>
     </div>
