99b4b45f3492963563374916f4ac7cff3d070fe0
ayale3 <ayanle876@gmail.com>
2025-04-20 12:00:15 +0300

    bilawga  design  header

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="">
       <Header />
       <main>
         <Routes>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { Button } from "./ui/button";
 
+// bg color #05203c
 export const Header = () => {
   return (
-    <div className="bg-[#05203c] shadow">
-      <div className="max-w-xl max-auto px-4 sm:px-6 lg:px-8 flex justify-between ">
-        <div className="bg-red-100 text-white p-4 rounded">
-          Dark blue background with white text
-        </div>
-        <Button variant="secondary" size="lg" className="mt-2">
-          Hello
-        </Button>
-        <div className="bg-[#05203c] text-white p-4 rounded-lg">
-  This background is baluu!
-</div>
-        header
-      </div>
+    <div className="max-w-full bg-[#05203c]">
+      <header className="   mx-auto  h-24 flex   px-24  ">
+        <nav className="w-full flex  items-center justify-between">
+          {/* nav left */}
+          <div className=" ">
+            <h1 className="text-white  text-3xl font-semibold" >Dahabside</h1>
+          </div>
+
+          <div>
+            <h2>Login</h2>
+          </div>
+        </nav>
+      </header>
     </div>
   );
 };
